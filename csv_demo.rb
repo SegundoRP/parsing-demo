e53c6b981d926528718e18046951f94c2dd55ad0
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-08-02 23:58:13 -0500

    chore: use foreach method with headers

--- csv_demo.rb
+++ csv_demo.rb
@@ -3,7 +3,11 @@ require "csv"
 # TODO - let's read/write data from beatles.csv
 filepath = "data/beatles.csv"
 
-CSV.foreach(filepath) do |row|
-  # Here, row is an array of columns
-  puts "#{row[0]} | #{row[1]} | #{row[2]}"
+# CSV.foreach(filepath) do |row|
+#   # Here, row is an array of columns
+#   puts "#{row[0]} | #{row[1]} | #{row[2]}"
+# end
+
+CSV.foreach(filepath, headers: :first_row) do |row|
+  puts "#{row['First Name']} #{row['Last Name']} played #{row['Instrument']}"
 end
