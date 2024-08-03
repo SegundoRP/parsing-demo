cb86e66c9432ae4de365f343f4e5a5966acb7a76
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-08-03 00:10:33 -0500

    chore: storing json info in json file

--- json_demo.rb
+++ json_demo.rb
@@ -3,11 +3,33 @@ require "json"
 # TODO - let's read/write data from beatles.json
 filepath = "data/beatles.json"
 
-serialized_beatles = File.read(filepath)
+# Parsing
 
-beatles = JSON.parse(serialized_beatles)
+# serialized_beatles = File.read(filepath)
+
+# beatles = JSON.parse(serialized_beatles)
 
 # beatles is a ruby Hash
 
 # p serialized_beatles
 # p beatles['title']
+
+
+# Storing
+
+beatles = { beatles: [
+  {
+    first_name: "John",
+    last_name: "Lennon",
+    instrument: "Guitar"
+  },
+  {
+    first_name: "Paul",
+    last_name: "McCartney",
+    instrument: "Bass Guitar"
+  }
+]}
+
+File.open(filepath, "wb") do |file|
+  file.write(JSON.generate(beatles))
+end
