6e730c9d81f91c82fa46382a16bb7856a8179d93
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-08-02 23:59:19 -0500

    chore: use open method to store info in csv

--- csv_demo.rb
+++ csv_demo.rb
@@ -3,11 +3,21 @@ require "csv"
 # TODO - let's read/write data from beatles.csv
 filepath = "data/beatles.csv"
 
+# Parsing
+
 # CSV.foreach(filepath) do |row|
 #   # Here, row is an array of columns
 #   puts "#{row[0]} | #{row[1]} | #{row[2]}"
 # end
 
-CSV.foreach(filepath, headers: :first_row) do |row|
-  puts "#{row['First Name']} #{row['Last Name']} played #{row['Instrument']}"
+# CSV.foreach(filepath, headers: :first_row) do |row|
+#   puts "#{row['First Name']} #{row['Last Name']} played #{row['Instrument']}"
+# end
+
+# Storing
+
+CSV.open(filepath, "wb") do |csv|
+  csv << ["First Name", "Last Name", "Instrument"]
+  csv << ["John", "Lennon", "Guitar"]
+  csv << ["Paul", "McCartney", "Bass Guitar"]
 end
