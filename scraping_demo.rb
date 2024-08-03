6a12c23e6254047b0f5ff6507a90e1e9a86b33e5
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-08-03 00:32:54 -0500

    chore: fetch data from scraping web

--- scraping_demo.rb
+++ scraping_demo.rb
@@ -2,3 +2,14 @@ require "open-uri"
 require "nokogiri"
 
 # Let's scrape recipes from https://www.bbcgoodfood.com
+
+ingredient = "chocolate"
+url = "https://www.bbcgoodfood.com/search/recipes?q=#{ingredient}"
+
+html_file = URI.open(url).read
+html_doc = Nokogiri::HTML.parse(html_file)
+
+html_doc.search(".layout-md-rail__primary .card__content a").each do |element|
+  puts element.text.strip
+  puts element.attribute("href").value
+end
