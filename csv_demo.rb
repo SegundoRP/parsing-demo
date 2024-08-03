218d392b9fbd4b904fcc75491dfac3d6dc06beb1
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-08-02 23:57:04 -0500

    chore: use foreach method to iterate in csv

--- csv_demo.rb
+++ csv_demo.rb
@@ -2,3 +2,8 @@ require "csv"
 
 # TODO - let's read/write data from beatles.csv
 filepath = "data/beatles.csv"
+
+CSV.foreach(filepath) do |row|
+  # Here, row is an array of columns
+  puts "#{row[0]} | #{row[1]} | #{row[2]}"
+end
