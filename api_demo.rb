be5a24b4e45ff779977d246f727ade2b0cce4f71
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-08-03 00:15:26 -0500

    chore: fetch data from api

--- api_demo.rb
+++ api_demo.rb
@@ -3,3 +3,8 @@ require "open-uri"
 
 # TODO - Let's fetch name and bio from a given GitHub username
 url = "https://api.github.com/users/ssaunier"
+
+user_serialized = URI.open(url).read
+user = JSON.parse(user_serialized)
+
+puts "#{user["name"]} - #{user["bio"]}"
