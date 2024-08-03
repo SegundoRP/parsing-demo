994f753cf777adcf747d929705e6fbce5fa2bfc1
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-08-03 00:01:30 -0500

    chore: parse to get info from json file

--- json_demo.rb
+++ json_demo.rb
@@ -2,3 +2,12 @@ require "json"
 
 # TODO - let's read/write data from beatles.json
 filepath = "data/beatles.json"
+
+serialized_beatles = File.read(filepath)
+
+beatles = JSON.parse(serialized_beatles)
+
+# beatles is a ruby Hash
+
+# p serialized_beatles
+# p beatles['title']
